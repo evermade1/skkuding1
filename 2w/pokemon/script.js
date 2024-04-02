52d3ba400649857d99553c503afb32a901a99435
evermade1 <scmy1013@naver.com>
2024-04-03 01:18:13 +0900

    2w

---
+++ 2w/pokemon/script.js
@@ -0,0 +1,25 @@
+const pokemon = JSON.parse(window.localStorage.getItem("pokemon"));
+
+const main = document.querySelector("main");
+
+const subdetail = document.createElement("div");
+subdetail.classList.add("subdetail");
+const img = document.createElement("img");
+img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
+img.alt = pokemon.name;
+subdetail.appendChild(img);
+const table = document.createElement("table");
+const tbody = document.createElement("tbody");
+table.appendChild(tbody);
+
+Object.keys(pokemon).forEach((key) => {
+  const tr = document.createElement("tr");
+  const td = document.createElement("td");
+  td.innerText = key;
+  const tdValue = document.createElement("td");
+  tdValue.innerText = pokemon[key];
+  tr.append(td, tdValue);
+  tbody.appendChild(tr);
+});
+
+main.append(subdetail, table);
